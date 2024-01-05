682733e7306a99ad9344c778cc872cc060589a18
MaouStudent <134606051+MaouStudent@users.noreply.github.com>
2024-01-05 17:49:04 +0700

    Update ESP_FRONTLINES.lua

--- ESP_FRONTLINES.lua
+++ ESP_FRONTLINES.lua
@@ -3,10 +3,6 @@ loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/
 -- Anti AFK
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
-
-repeat
-    wait()
-until game:IsLoaded()
 wait()
 Player.Idled:connect(function()
     game:GetService("VirtualUser"):ClickButton2(Vector2.new())
