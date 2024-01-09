f1e43df62e6ba469014036efb7fe78c77d39cb6f
MaouStudent <65011212122@msu.ac.th>
2024-01-09 12:02:52 +0700

    Update Timber2.lua

--- Timber2.lua
+++ Timber2.lua
@@ -26,20 +26,13 @@ local function getTrees()
             end) then
                 local id = tonumber(object.Name)
                 local logs = object:GetAttribute("Logs")
-                table.insert(trees, {
-                    id = id,
-                    logs = logs,
-                    part = object,
-                    primaryPart = object.PrimaryPart
-                })
+                table.insert(trees, {id = id,logs = logs,part = object,primaryPart = object.PrimaryPart})
             end
         end
     end
 
     -- order by Logs
-    table.sort(trees, function(a, b)
-        return a.logs > b.logs
-    end)
+    table.sort(trees, function(a, b) return a.logs > b.logs end)
 
     return trees
 end
