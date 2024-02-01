f26f4d959d0474c32ea978e0326585f22093a87d
MaouStudent <65011212122@msu.ac.th>
2024-02-01 11:52:39 +0700

    test

--- pack.lua
+++ pack.lua
@@ -1,3 +1,5 @@
--- Version 3(unoffical) Code:
+-- -- Version 3(unoffical) Code:
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/78n/SimpleSpy/main/SimpleSpySource.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+-- local urlRaw = "https://raw.githubusercontent.com/shayrbx/Shrexplorer/main/ShrexMain.lua"
+-- loadstring(game:HttpGet(urlRaw))()
